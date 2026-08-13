1f893921386bbf806d0dd7b8ad6f2a0dc9f0fc74
Sanit Kumar Amartya <sanit.connect01@gmail.com>
2026-08-13 12:40:28 +0530

    feat: add application error handling

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).send("Internal Server Error");
+});
+
 module.exports = app;
