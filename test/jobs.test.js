1f23ae85fd57f482194bfa82993fc1edcb28685f
Sanit Kumar Amartya <sanit.connect01@gmail.com>
2026-08-16 07:34:10 +0530

    test: define job submission contract

--- test/jobs.test.js
+++ test/jobs.test.js
@@ -3,12 +3,16 @@ const request = require("supertest");
 const app = require("../src/app");
 
 describe("Job API", () => {
-  test("accepts a job submission", async () => {
-    const response = await request(app).post("/jobs").send({
-      name: "example-job",
-    });
+  test("accepts a job submission according to the API contract", async () => {
+    const response = await request(app)
+      .post("/jobs")
+      .set("Content-Type", "application/json")
+      .send({
+        name: "example-job",
+      });
 
     expect(response.status).toBe(202);
+    expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.body).toEqual({
       name: "example-job",
     });
